dccbb6de448f01082e26f532d9ed6b72eb40b165
zetmei <jameikao@gmail.com>
2017-06-01 17:44:00 +0800

    Add files via upload

--- js/layout.js
+++ js/layout.js
@@ -188,27 +188,45 @@ $(document).ready(function(){
 //     });
 // });
 
-(function($) {
-    $.fn.clickToggle = function(func1, func2) {
-        var funcs = [func1, func2];
-        this.data('toggleclicked', 0);
-        this.click(function() {
-            var data = $(this).data();
-            var tc = data.toggleclicked;
-            $.proxy(funcs[tc], this)();
-            data.toggleclicked = (tc + 1) % 2;
-        });
-        return this;
-    };
-}(jQuery));
+// (function($) {
+//     $.fn.clickToggle = function(func1, func2) {
+//         var funcs = [func1, func2];
+//         this.data('toggleclicked', 0);
+//         this.click(function() {
+//             var data = $(this).data();
+//             var tc = data.toggleclicked;
+//             $.proxy(funcs[tc], this)();
+//             data.toggleclicked = (tc + 1) % 2;
+//         });
+//         return this;
+//     };
+// }(jQuery));
+
+
+// $(document).ready(function(){  
+// $('.btnPlay').clickToggle(function() {
+//     // $('.slider').slickPlay();
+//     $('#otherTab1 .slider').slickPause();
+// }, function() {
+//     $('#otherTab1 .slider').slickPause();
+// });
+
+// });
 
 
 $(document).ready(function(){  
-$('.btnPlay').clickToggle(function() {
-    // $('.slider').slickPlay();
-    $('#otherTab1 .slider').slickPause();
-}, function() {
-    $('#otherTab1 .slider').slickPause();
-});
+
+    // $('.btnPlay').click(function () {
+    //   $('#otherTab1 .slider').slickPause();
+    // });
+  $('.btnPause').click(function() {
+      $('.regular').slick('slickPause');
+  });
+  $('.btnPlay').click(function() {
+      $('.regular').slick('slickPlay');
+  });
+  // $('#otherTab1 .btnPlay').click(function() {
+  //   $('.regular').slick('slickPlay');
+  // });
 
 });
